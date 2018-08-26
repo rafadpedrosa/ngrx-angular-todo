3560ea58b143b772af29d9fab24f7b8fda561d71
rafadpedrosa <rafadpedrosa@gmail.com>
2018-08-26 11:08:27 -0300

    removing unecessary import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
