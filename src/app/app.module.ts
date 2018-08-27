74cb1b1ba1ce56ce98fc04b06a8085a898b705d2
rafadpedrosa <rafadpedrosa@gmail.com>
2018-08-26 23:07:41 -0300

    adding ngrx router store configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
-import {
-  HTTP_INTERCEPTORS,
-  HttpClientModule
-} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  RouterModule,
-  Routes
-} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
+import { RouterStateSerializer, StoreRouterConnectingModule } from "@ngrx/router-store";
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
@@ -22,10 +17,8 @@ import { AuthModule } from './auth/auth.module';
 import { LoginGuard } from './auth/login.guard';
 import { PageNotFoundComponent } from './page-not-found-component/page-not-found-component';
 import { PrivateComponent } from './private/private.component';
-import {
-  metaReducers,
-  reducers
-} from './reducers';
+import { metaReducers, reducers } from './reducers';
+import { CustomSerializer } from "./shared/store.router.custom";
 
 const appRoutes: Routes = [
   {
@@ -66,17 +59,21 @@ const appRoutes: Routes = [
     AuthModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production
+      ? StoreDevtoolsModule.instrument()
+      : []
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true,
-    }
+    },
+    { provide: RouterStateSerializer, useClass: CustomSerializer }
   ],
   bootstrap: [ AppComponent ],
   exports: []
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,3 +1,4 @@
+import { routerReducer } from '@ngrx/router-store';
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { storeFreeze } from 'ngrx-store-freeze';
 import { environment } from '../../environments/environment';
@@ -6,6 +7,7 @@ import { authReducer } from '../auth/redux/auth.reducer';
 export interface State {}
 
 export const reducers: ActionReducerMap<State> = {
+  router: routerReducer,
   auth: authReducer
 };
 
