980c2e0ddbc0f6affc14f155b4a2aaaaf127cac8
rafadpedrosa <rafadpedrosa@gmail.com>
2018-09-09 15:58:20 -0300

    adding search to list TODO

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, switchMap } from 'rxjs/operators';
 
 import { Task } from '../model/task.model';
 
@@ -7,13 +9,51 @@ import { Task } from '../model/task.model';
   templateUrl: './task.component.html',
   styleUrls: [ './task.component.css' ]
 })
-export class TaskComponent implements OnInit {
+export class TaskComponent implements OnInit, OnDestroy {
 
-  @Input() tasks: Task[];
+  filteredTasks: Task[];
+  searchInput: string;
+  search$ = new Subject<string>();
+
+  private tasks: Task[];
 
   constructor() {
   }
 
-  ngOnInit() {
+  @Input() set tasksData(tasks) {
+    this.tasks = tasks;
+    this.filteredTasks = tasks;
   }
+
+  search(event) {
+    const searchValue = event.target.value.toUpperCase();
+    this.search$.next(searchValue);
+  }
+
+  ngOnInit() {
+    this.filteredTasks = this.tasks;
+    this.initSearch();
+  }
+
+  ngOnDestroy() {
+  }
+
+  private initSearch() {
+    const filterTasks = searchValue => {
+      const searchValuePattern = `${searchValue}`;
+      this.filteredTasks = [];
+      return this.tasks.filter(task => task.title.toUpperCase().match(searchValuePattern));
+    };
+
+    const setFilteredTasks = tasks => {
+      console.log('tasks', tasks);
+      this.filteredTasks.push(tasks);
+    };
+
+    this.search$.pipe(
+      debounceTime(300),
+      switchMap(filterTasks),
+    ).subscribe(setFilteredTasks);
+  }
+
 }
